b24143a563e2c92fc6148dc48bf4b4d6a8ecd631
Sefora Rodriguez <seforarodriguez13@gmail.com>
2022-09-15 15:05:54 -0400

    added propertiesList to show my properties

--- suds/src/home/Propertieslist.js
+++ suds/src/home/Propertieslist.js
@@ -1,29 +1,43 @@
+import { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 
 
 
 export const PropertiesList = () => {
 
+    const [properties, setProperties] = useState([])
 
+    //this is fetching the properties
+    useEffect(
+        () => {
+            fetch(' http://localhost:8088/properties')
+                .then(response => response.json())
+                .then((propertiesArray) => {
+                    setProperties(propertiesArray)
+                })
+        },
+        [] // When this array is empty, you are observing initial component state
+    )
 
+    return <>
+        <h2> My Properties </h2>
+        <section className="allProperties">
+            {
+                properties.map(property => {
+                    return <div>
+                        <ul>
+                            <li>{property.name}</li>
+                            <li>{property.street}, {property.city} {property.zipCode}</li>
+                            <li>{property.houseSize}</li>
+                        </ul>
+                        <Button variant="secondary">Delete</Button>{' '}
+                        <Button variant="secondary">Edit</Button>{' '}
+                    </div>
 
-return <>
-<h2> My Properties </h2>
-<section className="allProperties">
-    <div>
-        <ul>
-            <li> property's Image</li>
-            <li> Cleaning Status</li>
-            <li> Property's address</li>
-            <li> Date needed</li>
-        </ul>
-        <Button variant="secondary">Delete</Button>{' '}
-        <Button variant="secondary">Edit</Button>{' '}
-        Delete button
-        Edit button
-    </div>
-</section>
+                })
+            }
+        </section>
 
-</>
+    </>
 
 } //closing bracket
