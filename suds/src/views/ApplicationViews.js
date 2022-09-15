13f1c5479b11f026777a1532a390dc8da5d93042
Sefora Rodriguez <seforarodriguez13@gmail.com>
2022-09-15 11:30:31 -0400

    login set, and path rendered correctly

--- suds/src/views/ApplicationViews.js
+++ suds/src/views/ApplicationViews.js
@@ -1,13 +1,10 @@
 import { Outlet, Route, Routes } from "react-router-dom"
-// import { TicketForm } from "../serviceTickets/TicketForm"
-// import { TicketContainer } from "../tickets/TicketContainer"
-// import { TicketList } from "../tickets/TicketList"
-// import { TicketSearch } from "../tickets/TicketSearch"
+
 
 export const ApplicationViews = () => {
 	return (
         <Routes>
-            <Route path="/" element={
+            <Route path="/loggedin" element={
                 <>
                     <h1>Suds: Cleaning Made Easy</h1>
                     <div>your best Helper!</div>
