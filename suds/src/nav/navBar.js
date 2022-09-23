458cfd63c46ec310dd0e48c8d3a679d43e279fe6
Sefora Rodriguez <seforarodriguez13@gmail.com>
2022-09-23 09:52:54 -0400

    added style to navbar, and buttons looking airbnbiish

--- suds/src/nav/navBar.js
+++ suds/src/nav/navBar.js
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link, useNavigate } from 'react-router-dom';
+import "./navBar.css"
 
 
 
@@ -10,27 +11,32 @@ function NavBar() {
     const SudsUserObject = JSON.parse(localSudsUser)
 
     const navigate = useNavigate()
-    return <>
-        <ul className="navbar">
-            
-           {
-               SudsUserObject.host ? 
-            <>
-               <li className="navbar__item active"><Link className="navbar__link" to={`/myProperties`}>My Properties</Link></li>
-               <li><Link className="navbar__link" to="/createNewProperty"> + </Link></li>
-            </>
-                :<li className="navbar__item active"><Link className="navbar__link" to={`/myProperties`}>Dirty Properties</Link></li> 
-           }
-            {
-                localStorage.getItem("suds_user")
-                    ? <Nav.Link href="#logout" to="/login" onClick={() => {
-                        localStorage.removeItem("suds_user")
-                        navigate("/", { replace: true })
-                    }}>Logout</Nav.Link>
-                    : ""
-            }
-        </ul>
+    return<>
+        <header class="header">
+        <h1 class="logo"><a href="#">Suds:Cleaning Made Easy</a></h1>
+      
+      
+            <ul class="main-nav">
+                {
+                    SudsUserObject.host ?
+                        <>
+                            <li className="navbar__item active"><Link className="navbar__link" to={`/myProperties`}>My Properties</Link></li>
+                            <li><Link className="navbar__link" to="/createNewProperty"> + New Property </Link></li>
+                        </>
+                        : <li className="navbar__item active"><Link className="navbar__link" to={`/myProperties`}>Dirty Properties</Link></li>
+                }
+                {
+                    localStorage.getItem("suds_user")
+                        ? <Nav.Link href="#logout" to="/login" onClick={() => {
+                            localStorage.removeItem("suds_user")
+                            navigate("/", { replace: true })
+                        }}>Logout</Nav.Link>
+                        : ""
+                }
+            </ul>
+        </header>
     </>
+
 }
 
 export default NavBar;
